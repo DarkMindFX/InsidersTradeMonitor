79cfb79a70e84b683f5560bf06cdcf367de2e106
DarkMindFX <contact@darkmindfx.com>
2023-03-25 20:18:44 +0100

    Adding view for non-derivative transactions

--- Database/storprocs/Reports/p_Report_NonDerivTransactionsByDate.sql
+++ Database/storprocs/Reports/p_Report_NonDerivTransactionsByDate.sql
@@ -1,4 +1,4 @@
-SET ANSI_NULLS ON
+﻿SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
@@ -20,28 +20,25 @@ BEGIN
 	SELECT
 		ndo.ID,
 		ndo.Form4ReportID,
-		f4r.ReportID as SECReportID,
+		ndo.SECReportID,
 		ndo.TitleOfSecurity,
 		ndo.TransactionDate,
 		ndo.DeemedExecDate,
 		ndo.TransactionCodeID,
-		tc.Code as TrasnactionCode,
+		ndo.TrasnactionCode,
 		ndo.EarlyVoluntarilyReport,
 		ndo.SharesAmount,
 		ndo.TransactionTypeID,
-		tt.Code as TransactionType,
+		ndo.TransactionType,
 		ndo.Price,
 		ndo.AmountFollowingReport,
 		ndo.OwnershipTypeID,
-		ot.Code as OwnershipType,
+		ndo.OwnershipType,
 		ndo.NatureOfIndirectOwnership
-	FROM		dbo.NonDerivativeTransaction ndo
-	INNER JOIN	dbo.Form4Report f4r ON f4r.ID = ndo.Form4ReportID
-	INNER JOIN  dbo.TransactionCode tc ON tc.ID = ndo.TransactionCodeID
-	INNER JOIN	dbo.TransactionType tt ON tt.ID = ndo.TransactionTypeID
-	INNER JOIN	dbo.OwnershipType	ot ON ot.ID = ndo.OwnershipTypeID
-	INNER JOIN	dbo.Entity			e	ON e.ID = f4r.ReporterID
-	WHERE f4r.IssuerID = @IssuerID AND f4r.Date = @Date
+	FROM		
+		dbo.v_NonDerivativeTransaction ndo
+	
+	WHERE ndo.IssuerID = @IssuerID AND ndo.Date = @Date
 
 	
 	SET NOCOUNT ON;
