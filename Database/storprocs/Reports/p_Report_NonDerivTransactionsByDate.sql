e1cee96ca402e99981f3bd4655187c90c94b3305
DarkMindFX <contact@darkmindfx.com>
2023-03-25 20:02:00 +0100

    Adding storproc to report NonDeriv Transactions By Date for given company

--- Database/storprocs/Reports/p_Report_NonDerivTransactionsByDate.sql
+++ Database/storprocs/Reports/p_Report_NonDerivTransactionsByDate.sql
@@ -16,6 +16,33 @@ CREATE PROCEDURE [dbo].[p_Report_NonDerivTransactionsByDate]
 		@Date		DATETIME
 AS
 BEGIN
+
+	SELECT
+		ndo.ID,
+		ndo.Form4ReportID,
+		f4r.ReportID as SECReportID,
+		ndo.TitleOfSecurity,
+		ndo.TransactionDate,
+		ndo.DeemedExecDate,
+		ndo.TransactionCodeID,
+		tc.Code as TrasnactionCode,
+		ndo.EarlyVoluntarilyReport,
+		ndo.SharesAmount,
+		ndo.TransactionTypeID,
+		tt.Code as TransactionType,
+		ndo.Price,
+		ndo.AmountFollowingReport,
+		ndo.OwnershipTypeID,
+		ot.Code as OwnershipType,
+		ndo.NatureOfIndirectOwnership
+	FROM		dbo.NonDerivativeTransaction ndo
+	INNER JOIN	dbo.Form4Report f4r ON f4r.ID = ndo.Form4ReportID
+	INNER JOIN  dbo.TransactionCode tc ON tc.ID = ndo.TransactionCodeID
+	INNER JOIN	dbo.TransactionType tt ON tt.ID = ndo.TransactionTypeID
+	INNER JOIN	dbo.OwnershipType	ot ON ot.ID = ndo.OwnershipTypeID
+	INNER JOIN	dbo.Entity			e	ON e.ID = f4r.ReporterID
+	WHERE f4r.IssuerID = @IssuerID AND f4r.Date = @Date
+
 	
 	SET NOCOUNT ON;
 END
